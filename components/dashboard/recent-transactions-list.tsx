0c3b151f6aa9d5ce3e9759ff9ed7da1c8ef55a5e
agwadan <agwa.dan@gmail.com>
2026-01-23 10:31:27 +0300

    Refactor RecentTransactionsList to remove user info fetching and streamline transaction data retrieval from the API.

--- components/dashboard/recent-transactions-list.tsx
+++ components/dashboard/recent-transactions-list.tsx
@@ -4,7 +4,7 @@ import { Colors } from '@/constants/theme';
 import { mockCategories } from '@/data/mock-data';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { Transaction } from '@/types';
-import { fetchData, getUserInfo } from '@/utils';
+import { fetchData } from '@/utils';
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, StyleSheet, TouchableOpacity, View } from 'react-native';
 
@@ -21,11 +21,10 @@ export function RecentTransactionsList({ onViewAll }: RecentTransactionsListProp
     useEffect(() => {
         const loadTransactions = async () => {
             try {
-                const user = await getUserInfo();
-                const endpoint = user?.id ? `transactions?user=${user.id}` : 'transactions';
+                const endpoint = 'auth/transactions/';
                 const data = await fetchData(endpoint);
                 console.log('Data from server (raw):', data);
-                setTransactions(data.results)
+                setTransactions(data.results || data);
             } catch (error) {
                 console.error('Failed to load transactions:', error);
             } finally {
